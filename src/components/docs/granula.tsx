627b702a59b415c8060bde6fde3af7fbc9a0450b
Nurken <94748525+NurkenInc@users.noreply.github.com>
2023-10-08 15:38:27 +0600

    addition of granule info

---
+++ src/components/docs/granula.tsx
@@ -0,0 +1,9 @@
+export default function Granula() {
+  return (
+    <section id="docs-granula">
+      <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'># Granula</h2>
+      <p className='leading-7 [&:not(:first-child)]:mt-6'>Granule refers to the individual files that make up a collection or dataset. These granules are the basic units of data that can be searched, accessed, and downloaded from the Earthdata Search platform. They represent specific subsets of the larger dataset, often corresponding to a specific geographical area and time period.</p>
+      {/* react flow here */}
+    </section>
+  )
+}
