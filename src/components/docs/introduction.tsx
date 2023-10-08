20a042ecc2a0a69abf122f0cadf5057bd0e3c740
Mark <ingerkrut@gmail.com>
2023-10-08 14:42:05 +0600

    build fix

--- src/components/docs/introduction.tsx
+++ src/components/docs/introduction.tsx
@@ -2,7 +2,7 @@ export default function Introduction() {
   return (
     <section id="docs-intro">
       <h2 className='scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0'># Introduction</h2>
-      <p className='leading-7 [&:not(:first-child)]:mt-6'>Our platform leverages data from the NASA CMR API and NASA's Earthdata Search API/websites to provide geologists with a comprehensive, interactive, and user-friendly tool for accessing and analyzing geological data. We have integrated these data sources into an interactive Google Map, offering various viewing modes such as satellite, terrain, and plain.</p>
+      <p className='leading-7 [&:not(:first-child)]:mt-6'>Our platform leverages data from the NASA CMR API and NASA&apos;s Earthdata Search API/websites to provide geologists with a comprehensive, interactive, and user-friendly tool for accessing and analyzing geological data. We have integrated these data sources into an interactive Google Map, offering various viewing modes such as satellite, terrain, and plain.</p>
     </section>
   )
 }
